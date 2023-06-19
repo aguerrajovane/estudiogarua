126a20a381ae29500a465b082e2062492f15d1ac
Álvaro <alvaro@MacBook-Air-de-Luz.local>
2023-06-19 13:26:24 -0500

    fadeout animation added

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,50 @@
-import React from 'react'
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 
-const index = () => {
-    const router = useRouter();
+const IndexPage = () => {
+  const router = useRouter();
+  const containerRef = useRef(null);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      router.push('/es');
+      containerRef.current.classList.add('fade-out');
+
+      setTimeout(() => {
+        router.push('/es');
+      }, 500);
     }, 1000);
 
     return () => clearTimeout(timeout);
   }, []);
-  return (
-    <div className="bg-black min-h-screen flex items-center">
-        <img 
-            src="/logo.svg" 
-            alt="Logo" 
-            className="relative invert mx-3 h-4 lg:h-6 mx-auto" />
-    </div>
-  )
-}
 
-export default index
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <div ref={containerRef}>
+        <img
+          src="/logo.svg"
+          alt="Logo"
+          className="relative invert h-4 lg:h-6 mx-auto opacity-100"
+          key="logo"
+        />
+      </div>
+
+      <style jsx>{`
+        .fade-out {
+          animation: fade-out 0.5s ease-out;
+          opacity: 0;
+        }
+
+        @keyframes fade-out {
+          from {
+            opacity: 1;
+          }
+          to {
+            opacity: 0;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default IndexPage;
