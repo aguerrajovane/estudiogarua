03ed8600250cfc684189188a323adb545c953248
Álvaro <alvaro@MacBook-Air-de-Luz.local>
2023-06-21 12:35:38 -0500

    MobileMenuContacto added

---
+++ components/es/MobileMenuContacto.js
@@ -0,0 +1,146 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+const Modal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [animationClass, setAnimationClass] = useState('');
+  const [isLanguageMenuVisible, setIsLanguageMenuVisible] = useState(false);
+
+  const toggleModal = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsOpen(!isOpen);
+    }, 100); // Ajusta el tiempo de espera según tus necesidades
+  };
+
+  const toggleLanguageMenu = () => {
+    setIsLanguageMenuVisible(!isLanguageMenuVisible);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      setAnimationClass('slideInDown');
+    } else {
+      setAnimationClass('slideOutUp');
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 500); // Ajusta el tiempo de espera según la duración de la animación
+    }
+  }, [isOpen]);
+
+  return (
+    <>
+      {/* Botón de activación del modal */}
+      <button
+        className="w-12 h-12 flex items-center justify-center text-white stroke-black"
+        onClick={toggleModal}
+      >
+        {/* Ícono de hamburguesa */}
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M3 12h18M3 6h18M3 18h18" />
+        </svg>
+      </button>
+
+      {/* Modal */}
+      {isOpen && (
+        <div className={`fixed top-0 left-0 right-0 ${animationClass}`}>
+          <div className="flex items-center justify-center text-center">
+            <div className="fixed" onClick={toggleModal}></div>
+
+            <div className="bg-black text-white w-full mx-auto min-h-screen">
+              {/* Cabezera del modal */}
+              <div className="flex items-center justify-between pt-8 pb-6 w-full max-w-6xl mx-auto">
+                {/* Logotipo */}
+                <Link href="/es">
+                  <img src="/logo.svg" alt="Logo" className="relative invert mx-3 h-4 lg:h-6" />
+                </Link>
+                {/* Botón para cerrar el modal */}
+                <button className="pr-4 h-12 rounded focus:outline-none" onClick={toggleModal}>
+                {/* Ícono de "x" */}
+                <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M18 6L6 18M6 6l12 12" />
+                </svg>
+                </button>
+              </div>
+              {/* Contenido del modal */}
+              <div className="flex flex-col items-start justify-around px-4 h-screen pb-20">
+                <div className="flex w-1/2 right-0 py-6">
+                  {/* Lista de navegación */}
+                  <nav className="flex flex-col items-start gap-2">
+                    <Link
+                      href="/es/nosotros"
+                      className="block px-0 py-2 text-4xl hover:underline hover:underline-offset-8"
+                    >
+                      Estudio
+                    </Link>
+                    <Link
+                      href="/es/servicios"
+                      className="block px-0 py-2 text-4xl hover:underline hover:underline-offset-8"
+                    >
+                      Servicios
+                    </Link>
+                    <Link
+                      href="/es/portafolio"
+                      className="block px-0 py-2 text-4xl hover:underline hover:underline-offset-8"
+                    >
+                      Portafolio
+                    </Link>
+                    <Link
+                      href="/es/blog"
+                      className="block px-0 py-2 text-4xl hover:underline hover:underline-offset-8"
+                    >
+                      Blog
+                    </Link>
+                    <Link
+                      href="/es/contacto"
+                      className="block px-0 py-2 text-4xl hover:underline hover:underline-offset-8"
+                    >
+                      Contacto
+                    </Link>
+                  </nav>
+                </div>
+                {/* Footer */}
+                <div className="w-full max-w-6xl mx-auto">
+                    { /* Idioma */}
+                    <div className="flex text-white text-center pt-6 w-full max-w-6xl mx-auto">
+                        <Link href="/es">
+                            <button className="w-10 py-2 pr-4 mr-4">Es</button>
+                        </Link>
+                        <Link href="/en">
+                            <button className="w-full py-2 px-4 text-gray-500">En</button>
+                        </Link>
+                    </div>
+                    { /* Correo */}
+                    <a className="flex pb-10 pt-10" href="mailto:hola@estudiogarua.com">
+                    hola@estudiogarua.com
+                    </a>
+                </div>
+                
+                </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
--- components/es/NavbarContacto.js
+++ components/es/NavbarContacto.js
@@ -3,14 +3,12 @@ import Link from 'next/link';
 import dynamic from "next/dynamic";
 
 const DynamicDesktopMenuContacto= dynamic(() => import("../es/DesktopMenuContacto"), { ssr: false });
-
+const DynamicMobileMenuContacto= dynamic(() => import("../es/MobileMenuContacto"), { ssr: false });
 
 function Navbar() {
-  const [showMenuMobile, setShowMenuMobile] = useState(false);
+  const [showMenuMobile] = useState(false);
 
-  const toggleMenuMobile = () => {
-    setShowMenuMobile(!showMenuMobile);
-  };
+ 
 
   return (
     <nav className="z-10 sticky top-0 pb-6 pt-8 text-sm lg:flex">
@@ -18,20 +16,8 @@ function Navbar() {
         <Link href="/es">
           <img src="/logo.svg" alt="Logo" className="relative mx-3 h-4 lg:h-6" />
         </Link>
-        <div className="block lg:hidden justify-items-end">
-          <button
-            className="flex items-center px-3 py-2 rounded"
-            onClick={toggleMenuMobile}
-            >
-            <svg
-              className="fill-current h-5 w-5"
-              viewBox="0 0 20 20"
-              xmlns="<http://www.w3.org/2000/svg>"
-              >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
+        <div className="ml-12 mr-2">
+            <DynamicMobileMenuContacto />
         </div>
         <div
           className="w-full hidden block lg:flex lg:items-center lg:w-auto"
@@ -78,38 +64,7 @@ function Navbar() {
             showMenuMobile ? 'block' : 'hidden'
           } w-full block lg:hidden lg:flex lg:items-center lg:w-auto`}
           >
-          <div className="text-sm">
-            <Link
-              href="es/nosotros"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Estudio
-            </Link>
-            <Link
-              href="es/servicios"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Servicios
-            </Link>
-            <Link
-              href="es/portafolio"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Portafolio
-            </Link>
-            <Link
-              href="es/blog"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Blog
-            </Link>
-            <Link
-              href="es/contacto"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:px-2 lg:mt-0 lg:last:ml-0"
-              >
-              Contacto
-            </Link>
-          </div>
+          <DynamicMobileMenuContacto />
         </div>
     </nav>
     
