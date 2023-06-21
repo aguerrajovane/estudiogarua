71a7a30392a6f16817b3492e16c6416589e0db46
Álvaro <alvaro@MacBook-Air-de-Luz.local>
2023-06-21 12:17:26 -0500

    MobileMenu added

--- components/es/Navbar.js
+++ components/es/Navbar.js
@@ -3,14 +3,11 @@ import Link from 'next/link';
 import dynamic from "next/dynamic";
 
 const DynamicDesktopMenu= dynamic(() => import("../es/DesktopMenu"), { ssr: false });
-
+const DynamicMobileMenu= dynamic(() => import("../es/MobileMenu"), { ssr: false });
 
 function Navbar() {
-  const [showMenuMobile, setShowMenuMobile] = useState(false);
+  const [showMenuMobile] = useState(false);
 
-  const toggleMenuMobile = () => {
-    setShowMenuMobile(!showMenuMobile);
-  };
 
   return (
     <nav className="z-10 sticky top-0 pb-6 pt-8 text-sm lg:flex">
@@ -19,19 +16,9 @@ function Navbar() {
           <img src="/logo.svg" alt="Logo" className="relative invert mx-3 h-4 lg:h-6" />
         </Link>
         <div className="block lg:hidden justify-items-end">
-          <button
-            className="flex items-center px-3 py-2 rounded"
-            onClick={toggleMenuMobile}
-            >
-            <svg
-              className="fill-current h-5 w-5"
-              viewBox="0 0 20 20"
-              xmlns="<http://www.w3.org/2000/svg>"
-              >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
+          <div className="ml-12 mr-2">
+            <DynamicMobileMenu />
+          </div>
         </div>
         <div
           className="w-full hidden block lg:flex lg:items-center lg:w-auto"
@@ -78,38 +65,7 @@ function Navbar() {
             showMenuMobile ? 'block' : 'hidden'
           } w-full block lg:hidden lg:flex lg:items-center lg:w-auto`}
           >
-          <div className="text-sm">
-            <Link
-              href="es/nosotros"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Estudio
-            </Link>
-            <Link
-              href="es/servicios"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Servicios
-            </Link>
-            <Link
-              href="es/portafolio"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Portafolio
-            </Link>
-            <Link
-              href="es/blog"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:ml-4 lg:pr-4 lg:mt-0 lg:last:ml-0"
-              >
-              Blog
-            </Link>
-            <Link
-              href="es/contacto"
-              className="block text-left px-4 mt-6 lg:inline-block lg:mt-0 lg:px-2 lg:mt-0 lg:last:ml-0"
-              >
-              Contacto
-            </Link>
-          </div>
+          <DynamicMobileMenu />
         </div>
     </nav>
     
