57aab0553e2827b9c41feeb235449d7b8f2f48b5
aguerrajovane <alvaroguerrajovane@gmail.com>
2023-06-23 16:32:40 -0500

    portfolio pages added

--- pages/es/portafolio.js
+++ pages/es/portafolio.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { NextSeo } from 'next-seo';
 import Layout from '../../components/es/Layout.js'
+import Link from 'next/link.js';
+import Image from 'next/image'
 
+import MuseoNomada from '../../public/images/portafolio/museo-nomada/mn-tn-min.jpg'
+import CPRDV from '../../public/images/portafolio/centro-pastoral-red-de-vida/cprdv-tn-min.jpeg'
 const Portafolio = () => {
   return (
     <Layout> 
@@ -20,12 +24,103 @@ const Portafolio = () => {
         <header className="relative h-screen my-0 py-0">
           <h1 className="absolute bottom-60 text-5xl md:text-6xl font-bold leading-snug mb-4 fade-in-up">Nuestro trabajo</h1>
         </header>
-        {/* Contenido */}
-        <div className="w-full mt-10 sm:mt-0 md:w-1/2 h-full sm:min-h-screen flex flex-col justify-center">
-          <p className="text-base leading-relaxed	">Contenido</p>
-        <br />
-        
+        {/* Portafolio */}
+        <div className="w-full h-full flex flex-col justify-center">
+
+          {/* Proyecto 1 */}
+          <Link href="/es/portafolio/museo-nomada">
+            <div className="w-full flex flex-col justify-center sm:flex-row leading-normal md:leading-snug">
+              <div className="w-full sm:w-1/3">
+              <Image
+                src={MuseoNomada}
+                width={640}
+                height={640}
+                alt="Museo Nómada"
+              />
+              </div>
+              <div className="mt-4 ml-0 w-full sm:w-1/3 sm:ml-32 sm: flex flex-col justify-end">
+                <span className="text-xs">01</span>
+                <h3 className="text-2xl md:text-4xl mb-2 md:mb-6 mt-2 md:mt-6 hover:underline hover:underline-offset-8">Museo Nómada</h3>
+                <p className="text-xs">Ancón, Panamá</p>
+              </div>
+            </div>
+          </Link>
+          
+          {/* Proyecto 2 */}
+          <Link href="/es/portafolio/centro-pastoral-red-de-vida">
+            <div className="w-full flex flex-col justify-center sm:flex-row leading-normal md:leading-snug my-6">
+              <div className="w-full sm:w-1/3">
+              <Image
+                src={CPRDV}
+                width={640}
+                height={640}
+                alt="Centro Pastoral Red de Vida"
+              />
+              </div>
+              <div className="mt-4 ml-0 w-full sm:w-1/3 sm:ml-32 sm: flex flex-col justify-end">
+                <span className="text-xs">02</span>
+                <h3 className="text-2xl md:text-4xl mb-2 md:mb-6 mt-2 md:mt-6 hover:underline hover:underline-offset-8">Centro Pastoral Red de Vida</h3>
+                <p className="text-xs">Santa Fe, Darién</p>
+              </div>
+            </div>
+          </Link>
+
+           {/* Proyecto 3 */}
+           <Link href="/es/portafolio/mayukwayukwa-refugee-camp">
+            <div className="w-full flex flex-col justify-center sm:flex-row leading-normal md:leading-snug my-6">
+              <div className="w-full sm:w-1/3">
+              <Image
+                src={CPRDV}
+                width={640}
+                height={640}
+                alt="Mayukwayukwa Refugee Camp"
+              />
+              </div>
+              <div className="mt-4 ml-0 w-full sm:w-1/3 sm:ml-32 sm: flex flex-col justify-end">
+                <span className="text-xs">03</span>
+                <h3 className="text-2xl md:text-4xl mb-2 md:mb-6 mt-2 md:mt-6 hover:underline hover:underline-offset-8">Mayukwayukwa Refugee Camp</h3>
+                <p className="text-xs">Shibanga, África</p>
+              </div>
+            </div>
+          </Link>
+
+           {/* Proyecto 4 */}
+           <Link href="/es/portafolio/yoga-house-on-a-cliff">
+            <div className="w-full flex flex-col justify-center sm:flex-row leading-normal md:leading-snug my-6">
+              <div className="w-full sm:w-1/3">
+              <Image
+                src={CPRDV}
+                width={640}
+                height={640}
+                alt="Centro Pastoral Red de Vida"
+              />
+              </div>
+              <div className="mt-4 ml-0 w-full sm:w-1/3 sm:ml-32 sm: flex flex-col justify-end">
+                <span className="text-xs">04</span>
+                <h3 className="text-2xl md:text-4xl mb-2 md:mb-6 mt-2 md:mt-6 hover:underline hover:underline-offset-8">Yoga House on a Cliff</h3>
+                <p className="text-xs">Oleiros, Portugal</p>
+              </div>
+            </div>
+          </Link>
         </div>
+        <style jsx>{`
+        .fade-in-up {
+          animation: fade-in-up 0.8s ease-out;
+          opacity: 1;
+          transform: translateY(0);
+        }
+
+        @keyframes fade-in-up {
+          from {
+            opacity: 0;
+            transform: translateY(50px);
+          }
+          to {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+      `}</style>
       </main>
     </Layout>
   )
